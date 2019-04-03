29ec761dadbe229e0970aca6a33c3bf24f659b2c
David Třebický <davidtrebicky@gmail.com>
2019-04-03 18:46:14 +0200

    ignore desired count if autoscaled

--- ecs-app/modules/ecs-app/service.tf
+++ ecs-app/modules/ecs-app/service.tf
@@ -3,6 +3,7 @@ data "aws_ecs_task_definition" "application" {
 }
 
 resource "aws_ecs_service" "application" {
+  count   = "${var.autoscaling == "false" ? 1 : 0}"
   name    = "${var.name}"
   cluster = "${data.aws_ecs_cluster.ecs.arn}"
 
@@ -26,6 +27,42 @@ resource "aws_ecs_service" "application" {
     container_name   = "${var.name}"
     container_port   = "${var.port}"
     target_group_arn = "${aws_lb_target_group.application.arn}" # TODO - reference from this module
+  }
+
+  depends_on = ["aws_iam_role.ecs_task_execution"]
+
+  tags = "${var.tags}"
+}
+
+resource "aws_ecs_service" "application_autoscaled" {
+  count   = "${var.autoscaling == "true" ? 1 : 0}"
+  name    = "${var.name}"
+  cluster = "${data.aws_ecs_cluster.ecs.arn}"
+
+  # task_definition                  = "${aws_ecs_task_definition.application.family}:${aws_ecs_task_definition.application.revision}"
+  task_definition                    = "${aws_ecs_task_definition.application.family}:${max("${aws_ecs_task_definition.application.revision}", "${data.aws_ecs_task_definition.application.revision}")}"
+  desired_count                      = "${var.service_capacity}"
+  launch_type                        = "FARGATE"
+  deployment_maximum_percent         = "${var.max_healthy}"
+  deployment_minimum_healthy_percent = "${var.min_healthy}"
+  health_check_grace_period_seconds  = "${var.healthcheck_grace}"
+
+  network_configuration {
+    subnets         = ["${var.private_subnet_ids}"]
+    security_groups = ["${aws_security_group.application.id}"]
+
+    #    security_groups  = ["${var.security_group_ids}"]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    container_name   = "${var.name}"
+    container_port   = "${var.port}"
+    target_group_arn = "${aws_lb_target_group.application.arn}" # TODO - reference from this module
+  }
+
+  lifecycle {
+    ignore_changes = ["desired_count"]
   }
 
   depends_on = ["aws_iam_role.ecs_task_execution"]
