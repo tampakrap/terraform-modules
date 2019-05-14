54fe0fb9c4392ea1aa2f79b39b6035de0c8be05c
Theo Chatzimichos <chatzimichos@u.plus>
2019-05-14 15:21:42 +0200

    Add new modules for elasticache redis
    Adding two modules for redis:
    - elasticache-redis: encryption is disabled
    - elasticache-redis-enc: encryption is enabled, and the auth_token is
      mandatory
    
    The reason for adding two modules instead of one is that we can't have
    auth_token unset when encryption is disabled, which creates conflict on
    our workflow

---
+++ elasticache-redis/modules/elasticache-redis/main.tf
@@ -0,0 +1,36 @@
+locals {
+  name            = "${var.project}-${var.app}-${var.stage}"
+  name_underscore = "${var.project}_${var.app}_${var.stage}"
+
+  tags = "${
+    merge(
+      map(
+        "Name", "${local.name}",
+        "Project", "${var.project}",
+        "Stage", "${var.stage}",
+        "App", "${var.app}"
+      ), var.tags
+    )
+  }"
+}
+
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "${lower(local.name)}"
+  subnet_ids = ["${var.subnets}"]
+}
+
+resource "aws_elasticache_replication_group" "redis" {
+  replication_group_id          = "${lower(substr(local.name, 0, 20))}"
+  replication_group_description = "Replication group for Redis"
+  automatic_failover_enabled    = "${var.automatic_failover_enabled}"
+  number_cache_clusters         = "${var.desired_clusters}"
+  node_type                     = "${var.instance_type}"
+  parameter_group_name          = "${var.parameter_group}"
+  subnet_group_name             = "${aws_elasticache_subnet_group.default.name}"
+  security_group_ids            = ["${var.security_group_ids}"]
+  port                          = "${var.port}"
+  maintenance_window            = "${var.maintenance_window}"
+  at_rest_encryption_enabled    = "false"
+  transit_encryption_enabled    = "false"
+  tags                          = "${local.tags}"
+}
